548e5643565fe07cf6e2eb175f72991ff73f21ee
Melissa <m.lajtos@student.tugraz.at>
2022-09-18 16:03:07 +0200

    Adjusted code for my needs to generate ERDS maps.

--- src/ERDS.py
+++ src/ERDS.py
@@ -1,12 +1,14 @@
 import numpy as np
 import scipy.signal
 from matplotlib import pyplot as plt
+from matplotlib.colors import TwoSlopeNorm
 from more_itertools import locate
 
 
-def compute_erds(epochs, rois, fs, t_min, f_min=0, f_max=50, path=None):
+def compute_erds(epochs, rois, fs, t_min, f_min=0, f_max=50, path=None, subject='None', task=None):
     """
-    Function to compute ERDS maps for a set of epochs according to different regions of interest
+    Function to compute ERDS maps for a set of epochs according to different regions of interest.
+
     :param epochs: MNE epochs object containing the annotated epochs of interest with EEG channels
     :param rois: dict object containing a set of key-value pairs. The key must be the name of the region of
     interest, the value is a list containing the channels belonging to that ROI
@@ -45,26 +47,29 @@ def compute_erds(epochs, rois, fs, t_min, f_min=0, f_max=50, path=None):
     for roi in rois.keys():
         rois_numbers[roi] = np.array([epochs.ch_names.index(i) for i in rois[roi]])
 
-    # for each region of interest
-    for roi in rois_numbers.keys():
+    cnorm = TwoSlopeNorm(vmin=-1.5, vcenter=0, vmax=1.5)  # min, center & max ERDS
 
-        # extract the channels of interest
-        numbers = rois_numbers[roi]
-        roi_epochs = spectrogram[:, numbers, :, :]
+    # for each different condition found in the epochs
+    for condition in conditions:
+        fig, axes = plt.subplots(2, 2, figsize=(12, 8))
+        fig.suptitle(condition)
+        # for each region of interest
+        for i, roi in enumerate(rois_numbers.keys()):
 
-        # compute reference power
-        reference = np.squeeze(roi_epochs[:, :, :, np.argwhere(time < 0)])
-        power_reference = np.mean(reference, axis=-1)
+            # extract the channels of interest
+            numbers = rois_numbers[roi]
+            roi_epochs = spectrogram[:, numbers, :, :]
 
-        # compute ERDS maps according to the definition: for each epoch and for each channel, compute (A-R)/R for each
-        # time instant and each frequency, where A is the current power and R the reference power
-        for idx_epoch, epoch in enumerate(roi_epochs):
-            for idx_channel, channel in enumerate(epoch):
-                reference = power_reference[idx_epoch, idx_channel][:, np.newaxis]
-                roi_epochs[idx_epoch, idx_channel] = (channel - reference)/reference
+            # compute reference power
+            reference = np.squeeze(roi_epochs[:, :, :, np.argwhere(time < 0)])
+            power_reference = np.mean(reference, axis=-1)
 
-        # for each different condition found in the epochs
-        for condition in conditions:
+            # compute ERDS maps according to the definition: for each epoch and for each channel, compute (A-R)/R for each
+            # time instant and each frequency, where A is the current power and R the reference power
+            for idx_epoch, epoch in enumerate(roi_epochs):
+                for idx_channel, channel in enumerate(epoch):
+                    reference = power_reference[idx_epoch, idx_channel][:, np.newaxis]
+                    roi_epochs[idx_epoch, idx_channel] = (channel - reference) / reference
 
             # extract only the epochs of interest
             roi_condition_epochs = roi_epochs[list(locate(annotations, lambda x: x == condition))]
@@ -76,18 +81,21 @@ def compute_erds(epochs, rois, fs, t_min, f_min=0, f_max=50, path=None):
             # plot the ERDS map
             roi_condition_epochs = np.array(roi_condition_epochs)
             z_min, z_max = -np.abs(roi_condition_epochs).max(), np.abs(roi_condition_epochs).max()
-            fig, ax = plt.subplots()
-            p = ax.pcolor(x_axis, y_axis, roi_condition_epochs, cmap='RdBu', snap=True, vmin=z_min, vmax=z_max)
-            ax.set_xlabel('Time (s)')
-            ax.set_ylabel('Frequency (Hz)')
-            ax.set_title(condition + ' ' + roi)
+            # fig, ax = plt.subplots()
+            ax = axes[int(i/2)][0 if (i % 2) == 0 else 1]
+            p = ax.pcolor(x_axis, y_axis, roi_condition_epochs, cmap='RdBu', norm=cnorm, snap=True)
+            if i > 1:
+                ax.set_xlabel('Time (s)')
+            if (i % 2) == 0:
+                ax.set_ylabel('Frequency (Hz)')
+            ax.set_title(roi)
             ax.axvline(0, color='k')
             fig.colorbar(p, ax=ax)
-            if path is not None:
-                fig.savefig(path + '/' + condition + '_' + roi + '_erds.png')
-            else:
-                plt.show()
-            plt.close()
+        if path is not None:
+            fig.savefig(f'{path}/P{subject}_{task}_{condition}_erds.png')
+        else:
+            plt.show()
+        plt.close()
 
 
 def compute_erds_numpy(signals, annotations, channels_list, rois, fs, t_min, f_min=0, f_max=50, path=None):
@@ -145,7 +153,7 @@ def compute_erds_numpy(signals, annotations, channels_list, rois, fs, t_min, f_m
         for idx_epoch, epoch in enumerate(roi_epochs):
             for idx_channel, channel in enumerate(epoch):
                 reference = power_reference[idx_epoch, idx_channel][:, np.newaxis]
-                roi_epochs[idx_epoch, idx_channel] = (channel - reference)/reference
+                roi_epochs[idx_epoch, idx_channel] = (channel - reference) / reference
 
         # for each different condition found in the epochs
         for condition in conditions:
@@ -172,3 +180,20 @@ def compute_erds_numpy(signals, annotations, channels_list, rois, fs, t_min, f_m
             else:
                 plt.show()
             plt.close()
+
+
+if __name__ == '__main__':
+    import mne
+
+    subjects = ['001', '002', '003', '004', '005', '007', '008', '009', '010', '011', '012', '013', '014',
+                '015', '016', '017', '018', '019', '020', '021', '022', '023', '024', '025', '026', '027', '028',
+                '029', '030', '031']
+    tasks = ['ME', 'MI']
+    rois = {"C3": ["FC5", "FC1", "C3", "CP5", "CP1"], "C4": ["FC2", "FC6", "C4", "CP2", "CP6"],
+            "F-L": ["F3", "FC5", "FC1", "C3"], "F-R": ["F4", "FC6", "FC2", "C4"]}
+
+    for subject in subjects:
+        print('Subject: ', subject)
+        for task in tasks:
+            epochs = mne.read_epochs(f'../data/sub-P{subject}/sub-P{subject}_task-{task}-epo.fif')
+            compute_erds(epochs, rois, 500, -1.5, 1, 35, path=f'../images/sub-P{subject}', subject=subject, task=task)
