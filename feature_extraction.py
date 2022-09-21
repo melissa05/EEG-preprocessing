c7371fe13aad41244cd741dd8c8855c4bcf1d16a
Melissa <m.lajtos@student.tugraz.at>
2022-09-21 21:37:46 +0200

    Important commit!
    feature_extraction.py: There was a mistake in saving the ERDS values to the csv (Beta C3 and Alpha C4 were interchanged).
    The frequency interval for the alpha band was adjusted (from 8-14 Hz to 8-13 Hz).
    ERDS values are now rounded to two decimals.
    Else were just small changes not effecting the results.

--- feature_extraction.py
+++ feature_extraction.py
@@ -26,31 +26,25 @@ def calc_ERDS(epochs, channel):
             A = epochs_active.get_data(picks=[channel], item=e).mean()
             A_sum += A
             ERDS_sum += (A - R) / R
-        ERDS = ERDS_sum / epochs_event.events.shape[0]
+        ERDS = round(ERDS_sum * 100 / epochs_event.events.shape[0], 2)
         A = A_sum / epochs_event.events.shape[0]
 
-        # R_sum = epochs_base.get_data(picks=[channel], item=0)
-        # A_sum = epochs_active.get_data(picks=[channel], item=0)
-        # for e in range(1, epochs_event.events.shape[0]):
-        #     R_sum += epochs_base.get_data(picks=[channel], item=e)
-        #     A_sum += epochs_active.get_data(picks=[channel], item=e)
-        # R = R_sum.mean() / epochs_event.events.shape[0]
-        # A = A_sum.mean() / epochs_event.events.shape[0]
-        # ERDS = (A - R) / R
-
-        ERDS_dict[event] = ERDS * 100
+        ERDS_dict[event] = ERDS
         bandpower_dict[event] = A
+
     return ERDS_dict, bandpower_dict
 
 
 if __name__ == '__main__':
     subjects = ['001', '002', '003', '004', '005', '007', '008', '009', '010', '011', '012', '013', '014',
-                '015', '016', '017', '018', '019', '021', '022', '023', '024', '025', '026', '027', '028',
-                '029', '030', '031']  # Without 006 and 020
+                '015', '016', '017', '018', '019', '021', '022', '023', '024', '025', '026', '028',
+                '029', '030', '031']  # Without 006, 020 and 027
     # subjects = ['001', '002']
 
     # Creating the csv file to keep track of dropped epochs:
     dirname = 'C:\\Users\\Melissa\\PycharmProjects\\EEG-preprocessing\\data'
+
+    # ERDS values:
     if not os.path.isfile(f'{dirname}/erds.csv'):
         with open(f'{dirname}/erds.csv', 'w') as csvfile:
             writer = csv.writer(csvfile, delimiter=',', lineterminator='\n')
@@ -61,19 +55,20 @@ if __name__ == '__main__':
                              'MI_both_alpha_C3', 'MI_both_beta_C3', 'MI_both_alpha_C4', 'MI_both_beta_C4',
                              'MI_left_alpha_C3', 'MI_left_beta_C3', 'MI_left_alpha_C4', 'MI_left_beta_C4',
                              'MI_right_alpha_C3', 'MI_right_beta_C3', 'MI_right_alpha_C4', 'MI_right_beta_C4'])
+
+    # Bandpower values:
     if not os.path.isfile(f'{dirname}/bandpower.csv'):
         with open(f'{dirname}/bandpower.csv', 'w') as csvfile:
             writer = csv.writer(csvfile, delimiter=',', lineterminator='\n')
             writer.writerow(['participant',
-                             'ME_both_alpha_C3', 'ME_both_alpha_C4', 'ME_both_beta_C3', 'ME_both_beta_C4',
-                             'ME_left_alpha_C3', 'ME_left_alpha_C4', 'ME_left_beta_C3', 'ME_left_beta_C4',
-                             'ME_right_alpha_C3', 'ME_right_alpha_C4', 'ME_right_beta_C3', 'ME_right_beta_C4',
-                             'MI_both_alpha_C3', 'MI_both_alpha_C4', 'MI_both_beta_C3', 'MI_both_beta_C4',
-                             'MI_left_alpha_C3', 'MI_left_alpha_C4', 'MI_left_beta_C3', 'MI_left_beta_C4',
-                             'MI_right_alpha_C3', 'MI_right_alpha_C4', 'MI_right_beta_C3', 'MI_right_beta_C4'])
+                             'ME_both_alpha_C3', 'ME_both_beta_C3', 'ME_both_alpha_C4', 'ME_both_beta_C4',
+                             'ME_left_alpha_C3', 'ME_left_beta_C3', 'ME_left_alpha_C4', 'ME_left_beta_C4',
+                             'ME_right_alpha_C3', 'ME_right_beta_C3', 'ME_right_alpha_C4', 'ME_right_beta_C4',
+                             'MI_both_alpha_C3', 'MI_both_beta_C3', 'MI_both_alpha_C4', 'MI_both_beta_C4',
+                             'MI_left_alpha_C3', 'MI_left_beta_C3', 'MI_left_alpha_C4', 'MI_left_beta_C4',
+                             'MI_right_alpha_C3', 'MI_right_beta_C3', 'MI_right_alpha_C4', 'MI_right_beta_C4'])
 
     for subject in subjects:
-        print(f'Subject: P{subject}')
         results = {}
         for task in ['ME', 'MI']:
             epochs = mne.read_epochs(f'data/sub-P{subject}/sub-P{subject}_task-{task}-epo.fif', preload=True)
@@ -82,7 +77,7 @@ if __name__ == '__main__':
 
             # Alpha band:
             epochs_alpha = epochs_lap.copy()
-            epochs_alpha.filter(8, 14)
+            epochs_alpha.filter(8, 13)
             epochs_alpha.apply_function(square)
 
             ERDS_alpha_c3, bandpower_alpha_c3 = calc_ERDS(epochs_alpha, 'C3')
@@ -93,24 +88,24 @@ if __name__ == '__main__':
             print(f'P{subject} {task} alpha Bandpower: {bandpower_alpha_c4}')
 
             # Beta band:
-            beta = epochs_lap.copy()
-            beta.filter(16, 24)
-            beta.apply_function(square)
+            epochs_beta = epochs_lap.copy()
+            epochs_beta.filter(16, 24)
+            epochs_beta.apply_function(square)
 
-            ERDS_beta_c3, bandpower_beta_c3 = calc_ERDS(beta, 'C3')
+            ERDS_beta_c3, bandpower_beta_c3 = calc_ERDS(epochs_beta, 'C3')
             print(f'P{subject} {task} beta ERDS: {ERDS_beta_c3}')
             print(f'P{subject} {task} beta Bandpower: {bandpower_beta_c3}')
-            ERDS_beta_c4, bandpower_beta_c4 = calc_ERDS(beta, 'C4')
+            ERDS_beta_c4, bandpower_beta_c4 = calc_ERDS(epochs_beta, 'C4')
             print(f'P{subject} {task} beta ERDS: {ERDS_beta_c4}')
             print(f'P{subject} {task} beta Bandpower: {bandpower_beta_c4}')
 
             results[task] = {}
-            results[task]["ERDS"] = [ERDS_alpha_c3["both"], ERDS_alpha_c4["both"], ERDS_beta_c3["both"], ERDS_beta_c4["both"],
-                                       ERDS_alpha_c3["left"], ERDS_alpha_c4["left"], ERDS_beta_c3["left"], ERDS_beta_c4["left"],
-                                       ERDS_alpha_c3["right"], ERDS_alpha_c4["right"], ERDS_beta_c3["right"], ERDS_beta_c4["right"]]
-            results[task]["bandpower"] = [bandpower_alpha_c3["both"], bandpower_alpha_c4["both"], bandpower_beta_c3["both"], bandpower_beta_c4["both"],
-                                            bandpower_alpha_c3["left"], bandpower_alpha_c4["left"], bandpower_beta_c3["left"], bandpower_beta_c4["left"],
-                                            bandpower_alpha_c3["right"], bandpower_alpha_c4["right"], bandpower_beta_c3["right"], bandpower_beta_c4["right"]]
+            results[task]["ERDS"] = [ERDS_alpha_c3["both"], ERDS_beta_c3["both"], ERDS_alpha_c4["both"], ERDS_beta_c4["both"],
+                                     ERDS_alpha_c3["left"], ERDS_beta_c3["left"], ERDS_alpha_c4["left"], ERDS_beta_c4["left"],
+                                     ERDS_alpha_c3["right"], ERDS_beta_c3["right"], ERDS_alpha_c4["right"], ERDS_beta_c4["right"]]
+            results[task]["bandpower"] = [bandpower_alpha_c3["both"], bandpower_beta_c3["both"], bandpower_alpha_c4["both"], bandpower_beta_c4["both"],
+                                          bandpower_alpha_c3["left"], bandpower_beta_c3["left"], bandpower_alpha_c4["left"], bandpower_beta_c4["left"],
+                                          bandpower_alpha_c3["right"], bandpower_beta_c3["right"], bandpower_alpha_c4["right"], bandpower_beta_c4["right"]]
 
         with open(f'{dirname}/erds.csv', 'a') as csvfile:
             writer = csv.writer(csvfile, delimiter=',', lineterminator='\n')
